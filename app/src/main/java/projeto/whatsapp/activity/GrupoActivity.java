da578f84a838af8e8a8dabca8bc75e171b55068f
Gabriel George <biiel.gg22@gmail.com>
2020-09-30 15:46:06 -0300

    Adicionando a lista de contatos selecionados para a criação do grupo

--- app/src/main/java/projeto/whatsapp/activity/GrupoActivity.java
+++ app/src/main/java/projeto/whatsapp/activity/GrupoActivity.java
@@ -11,20 +11,23 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
+import android.widget.AdapterView;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
 import projeto.whatsapp.R;
 import projeto.whatsapp.adapter.ContatosAdapter;
+import projeto.whatsapp.adapter.GrupoSelecionadoAdapter;
 import projeto.whatsapp.config.ConfiguracaoFirebase;
+import projeto.whatsapp.helper.RecyclerItemClickListener;
 import projeto.whatsapp.helper.UsuarioFirebase;
 import projeto.whatsapp.model.Usuario;
 
@@ -32,11 +35,12 @@ public class GrupoActivity extends AppCompatActivity {
 
     private RecyclerView recyclerMembrosSelecionados, recyclerMembros;
     private ContatosAdapter contatosAdapter;
+    private GrupoSelecionadoAdapter grupoSelecionadoAdapter;
     private List<Usuario> listaMembros = new ArrayList<>();
+    private List<Usuario> listaMembrosSelecionados = new ArrayList<>();
     private ValueEventListener valueEventListenerMembros;
     private DatabaseReference usuariosRef;
     private FirebaseUser usuarioAtual;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,18 +62,93 @@ public class GrupoActivity extends AppCompatActivity {
         //Configurações Iniciais
         recyclerMembros = findViewById(R.id.recyclerMembros);
         recyclerMembrosSelecionados = findViewById(R.id.recyclerMembrosSelecionados);
+
         usuariosRef = ConfiguracaoFirebase.getFirebaseDatabase().child("usuarios");
         usuarioAtual = UsuarioFirebase.getUsuarioAtual();
 
         //Configura o adapter
         contatosAdapter = new ContatosAdapter(listaMembros, getApplicationContext());
-        //Configura recyclerView
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
 
+        //Configura recyclerView para os contatos
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext()); //dentro de activity
         recyclerMembros.setLayoutManager(layoutManager);
         recyclerMembros.setHasFixedSize(true);
         recyclerMembros.setAdapter(contatosAdapter);
 
+        recyclerMembros.addOnItemTouchListener(
+                new RecyclerItemClickListener(
+                        getApplicationContext(),
+                        recyclerMembros,
+                        new RecyclerItemClickListener.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(View view, int position) {
+
+                                Usuario usuarioSelecionado = listaMembros.get(position);
+
+                                //Remover usuário selecionado da lista
+                                listaMembros.remove(usuarioSelecionado);
+                                contatosAdapter.notifyDataSetChanged();
+
+                                //Adiciona usuario na nova lista de selecionados
+                                listaMembrosSelecionados.add(usuarioSelecionado);
+                                grupoSelecionadoAdapter.notifyDataSetChanged();
+                            }
+
+                            @Override
+                            public void onLongItemClick(View view, int position) {
+
+                            }
+
+                            @Override
+                            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                            }
+                        }
+                )
+        );
+
+        //Configurar recyclerView para os contatos selecionados
+        grupoSelecionadoAdapter = new GrupoSelecionadoAdapter(listaMembrosSelecionados, getApplicationContext());
+
+        RecyclerView.LayoutManager layoutManagerHorizontal = new LinearLayoutManager(
+                getApplicationContext(),
+                LinearLayoutManager.HORIZONTAL,
+                false //inverte a ordem de itens
+        );
+        recyclerMembrosSelecionados.setLayoutManager(layoutManagerHorizontal);
+        recyclerMembrosSelecionados.setHasFixedSize(true);
+        recyclerMembrosSelecionados.setAdapter(grupoSelecionadoAdapter);
+
+        recyclerMembrosSelecionados.addOnItemTouchListener(
+                new RecyclerItemClickListener(
+                        getApplicationContext(),
+                        recyclerMembrosSelecionados,
+                        new RecyclerItemClickListener.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(View view, int position) {
+                                Usuario usuarioSelecionado = listaMembrosSelecionados.get(position);
+
+                                //remover da listagem de membros selecionados
+                                listaMembrosSelecionados.remove(usuarioSelecionado);
+                                grupoSelecionadoAdapter.notifyDataSetChanged();
+
+                                //adicionar a listagem de membros
+                                listaMembros.add(usuarioSelecionado);
+                                contatosAdapter.notifyDataSetChanged();
+                            }
+
+                            @Override
+                            public void onLongItemClick(View view, int position) {
+
+                            }
+
+                            @Override
+                            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                            }
+                        }
+                )
+        );
     }
 
     public void recuperarContatos(){
