0c5f8acf17105e47674bb0d542716080ca7b4932
Gabriel George <biiel.gg22@gmail.com>
2020-10-05 22:09:18 -0300

    Configurando a CadastroGrupoActivity para salvar a imagem do grupo (parte 1)

--- app/src/main/java/projeto/whatsapp/activity/CadastroGrupoActivity.java
+++ app/src/main/java/projeto/whatsapp/activity/CadastroGrupoActivity.java
@@ -1,23 +1,32 @@
 package projeto.whatsapp.activity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.storage.StorageReference;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.TextView;
 
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import de.hdodenhof.circleimageview.CircleImageView;
 import projeto.whatsapp.R;
 import projeto.whatsapp.adapter.GrupoSelecionadoAdapter;
+import projeto.whatsapp.config.ConfiguracaoFirebase;
 import projeto.whatsapp.model.Usuario;
 
 public class CadastroGrupoActivity extends AppCompatActivity {
@@ -27,6 +36,8 @@ public class CadastroGrupoActivity extends AppCompatActivity {
     private GrupoSelecionadoAdapter grupoSelecionadoAdapter;
     private RecyclerView recyclerMembrosSelecionados;
     private CircleImageView imageGrupo;
+    private static final int SELECAO_GALERIA = 200;
+    private StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,9 +52,18 @@ public class CadastroGrupoActivity extends AppCompatActivity {
         textTotalParticipantes = findViewById(R.id.textTotalParticipantes);
         recyclerMembrosSelecionados = findViewById(R.id.recyclerMembrosGrupo);
         imageGrupo = findViewById(R.id.imageGrupo);
+        storageReference = ConfiguracaoFirebase.getFirebaseStorage();
         
         //Configurar evento de clique
-        imageGrupo.setOnClickListener();
+        imageGrupo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                if(i.resolveActivity(getPackageManager()) != null){//verificar se abriu a galeria
+                    startActivityForResult(i,SELECAO_GALERIA);
+                }
+            }
+        });
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -73,5 +93,40 @@ public class CadastroGrupoActivity extends AppCompatActivity {
         recyclerMembrosSelecionados.setLayoutManager(layoutManagerHorizontal);
         recyclerMembrosSelecionados.setHasFixedSize(true);
         recyclerMembrosSelecionados.setAdapter(grupoSelecionadoAdapter);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK){
+            Bitmap imagem = null;
+
+            try {
+                Uri localImagemSelecionada = data.getData();
+                imagem = MediaStore.Images.Media.getBitmap(getContentResolver(), localImagemSelecionada);
+
+                if (imagem != null){
+                    imageGrupo.setImageBitmap(imagem);
+
+                    //Recuperar dados da imagem para o firebase
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    imagem.compress(Bitmap.CompressFormat.JPEG, 70, baos);
+                    byte[] dadosImagem = baos.toByteArray();
+
+                    //Salvar imagem no firebase
+                    StorageReference imagemRef = storageReference
+                            .child("imagens")
+                            .child("grupos")
+                            .child("NOME PROVISORIO")
+                            .child("perfil.jpeg");
+                    final StorageReference imagemRef2 = imagemRef;
+
+
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 }
